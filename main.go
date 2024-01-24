f7244a15729d9f5f88e4646221ffceee38c3a3f9
Hamid <hamidosouli76@gmail.com>
2024-01-24 21:04:21 +0330

    chore: add basic http server

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in env")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("server starting on port %v", portString)
+
+	listenErr := server.ListenAndServe()
+	if listenErr != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println("PORT is:", portString)
 }
